325b7c217b6d2f5f8971d07e7d62cba66a76aabc
Den-codern <78246895+Den-codern@users.noreply.github.com>
2023-04-14 21:37:36 +0600

    :sparkles: Add search, get by region, add to local storage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,10 +5,17 @@ import {
   Button,
   Container,
   Box,
+  Text,
+  Skeleton,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 import { axiosInstance } from "../App";
 import { useState, useEffect } from "react";
-import { Countries } from "@service";
+import { Countries, LocalStorage } from "@service";
+import { Card } from "@components";
+import { Link } from "react-router-dom";
+import { FAVORITE_URL } from "@router";
 
 const HomePage = () => {
   const [search, setSearch] = useState<string>("");
@@ -16,6 +23,8 @@ const HomePage = () => {
   const [currentRegion, setCurrentRegion] = useState<string>("");
 
   const countriesService = new Countries(axiosInstance);
+  const storageService = new LocalStorage("countries");
+  const [countries, setCountries] = useState<any[]>([]);
 
   useEffect(() => {
     try {
@@ -27,15 +36,39 @@ const HomePage = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (currentRegion) {
+      try {
+        countriesService
+          .getCountriesByRegion(currentRegion.toLocaleLowerCase())
+          .then((countries) => {
+            setCountries(countries);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [currentRegion]);
+
   function onSearch() {
     if (search) {
-      countriesService.getCountriesByName(search);
+      setCurrentRegion("");
+      countriesService.getCountriesByName(search).then((countries) => {
+        setCountries(countries);
+      });
     }
   }
 
   function onRegionChange(region: string) {
     setCurrentRegion(region);
-    countriesService.getCountriesByRegion(region);
+  }
+  
+  function onAdd(country: any) {
+    storageService.push(country);
+  }
+
+  function onRemove(name: string) {
+    storageService.delete(name);
   }
 
   return (
@@ -43,15 +76,25 @@ const HomePage = () => {
       <Container maxW={"7xl"}>
         <Stack spacing={4}>
           <Flex gap={4} wrap="wrap">
-            {regions.length &&
+            {regions.length ? (
               regions.map((region) => (
                 <Button
                   isActive={currentRegion == region}
                   onClick={() => onRegionChange(region)}
+                  key={region}
                 >
                   {region}
                 </Button>
-              ))}
+              ))
+            ) : (
+              <Flex gap={6}>
+                <Skeleton w="32" h="40px"></Skeleton>
+                <Skeleton w="32" h="40px"></Skeleton>
+                <Skeleton w="32" h="40px"></Skeleton>
+                <Skeleton w="32" h="40px"></Skeleton>
+                <Skeleton w="32" h="40px"></Skeleton>
+              </Flex>
+            )}
           </Flex>
           <Flex align={"center"} gap="6">
             <Input
@@ -76,7 +119,42 @@ const HomePage = () => {
             </Button>
           </Flex>
         </Stack>
+
+        <Grid
+          mt={12}
+          gap={6}
+          gridTemplateColumns="repeat(auto-fit, minmax(375px, 1fr))"
+        >
+          {countries.length ? (
+            countries.map((country) => (
+              <GridItem key={country.name.common}>
+                <Card onAdd={onAdd} onRemove={onRemove} country={country} />
+              </GridItem>
+            ))
+          ) : (
+            <Text
+              color={"gray.500"}
+              fontWeight={600}
+              fontSize="6xl"
+              textTransform={"uppercase"}
+              mt={12}
+            >
+              Result {countries.length}
+            </Text>
+          )}
+        </Grid>
       </Container>
+      <Button
+        as={Link}
+        to={FAVORITE_URL}
+        position="fixed"
+        bottom={12}
+        left={6}
+        colorScheme="blue"
+        zIndex={99}
+      >
+        Избранные страны
+      </Button>
     </Box>
   );
 };
